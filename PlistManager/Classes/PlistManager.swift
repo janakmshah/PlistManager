a0ec48e350d208e5cde3cca6cc4c66ff6d15d8dd
Janak Shah <janak.shah@cuvva.com>
2020-04-24 23:24:31 +0100

    get value for key, specify plistName

--- PlistManager/Classes/PlistManager.swift
+++ PlistManager/Classes/PlistManager.swift
@@ -2,42 +2,84 @@ import AnyCodable
 
 public class PlistManager {
     
-    private static let documentsDirectory: URL = {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }()
+    public typealias PlistName = String
     
-    public static func get<T:Codable>(_ type: T.Type) throws -> T {
-        let path: URL = documentsDirectory.appendingPathComponent("\(String(describing: type.self)).plist")
-        writeFileIfNeeded(path)
+    private static func plistPath(_ name: PlistName) -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0].appendingPathComponent("\(name).plist")
+    }
+    
+    private static func createFileIfNeeded(_ path: URL) {
+        if !FileManager.default.fileExists(atPath: path.path) {
+            let emptyDict = NSDictionary(dictionary: [String: String]())
+            emptyDict.write(toFile: path.path, atomically: true)
+        }
+    }
+    
+    private static func getPlistContents(for path: URL) -> Result<[String: AnyCodable], Error> {
+        createFileIfNeeded(path)
         do {
             let data = try Data(contentsOf: path)
             let decoder = PropertyListDecoder()
-            return try decoder.decode(T.self, from: data)
+            let dict = try decoder.decode([String: AnyCodable].self, from: data)
+            return(.success(dict))
         } catch {
-            throw error
+            return(.failure(error))
         }
     }
     
-    public static func save<T:Codable>(_ value: T) throws {
-        let path: URL = documentsDirectory.appendingPathComponent("\(String(describing: T.self)).plist")
-        let encoder = PropertyListEncoder()
-        encoder.outputFormat = .xml
-        do {
-          let data = try encoder.encode(value)
-          try data.write(to: path)
-        } catch {
-          throw error
+    public static func getObject(_ key: String, from plistName: PlistName, handleError: ((Error?) -> Void)? = nil) -> AnyCodable? {
+        let path = plistPath(plistName)
+        switch getPlistContents(for: path) {
+        case .failure(let error):
+            handleError?(error)
+            return nil
+        case .success(let plistContents):
+            if let object = plistContents[key] {
+                handleError?(nil)
+                return object
+            } else {
+                handleError?(NSError()) //TODO: Make this a custom error
+                return nil
+            }
         }
     }
     
-    private static func writeFileIfNeeded(_ path: URL) {
-        if !FileManager.default.fileExists(atPath: path.path) {
-            let emptyDict = NSDictionary(dictionary: [String: String]())
-            let isWritten = emptyDict.write(toFile: path.path, atomically: true)
-            print("is the file created: \(isWritten)")
-        } else {
-            print("file exists")
+    public static func remove(for key: String, from plistName: PlistName, handleError: ((Error?) -> Void)? = nil) {
+        let path = plistPath(plistName)
+        switch getPlistContents(for: path) {
+        case .failure(let error):
+            handleError?(error)
+        case .success(var plistContents):
+            plistContents.removeValue(forKey: key)
+            let encoder = PropertyListEncoder()
+            encoder.outputFormat = .xml
+            do {
+                let data = try encoder.encode(plistContents)
+                try data.write(to: path)
+                handleError?(nil)
+            } catch {
+                handleError?(error)
+            }
+        }
+    }
+    
+    public static func saveValue(_ value: AnyCodable, key: String, from plistName: PlistName, handleError: ((Error?) -> Void)? = nil) {
+        let path = plistPath(plistName)
+        switch getPlistContents(for: path) {
+        case .failure(let error):
+            handleError?(error)
+        case .success(var plistContents):
+            plistContents[key] = value
+            let encoder = PropertyListEncoder()
+            encoder.outputFormat = .xml
+            do {
+                let data = try encoder.encode(plistContents)
+                try data.write(to: path)
+                handleError?(nil)
+            } catch {
+                handleError?(error)
+            }
         }
     }
 
