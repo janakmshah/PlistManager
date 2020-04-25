de36d93676f011f41b241fae16ef9aa76fe6b90b
Janak Shah <janak.shah@cuvva.com>
2020-04-25 21:44:05 +0100

    save Codable object to Plist and back again

--- PlistManager/Classes/PlistManager.swift
+++ PlistManager/Classes/PlistManager.swift
@@ -1,5 +1,3 @@
-import AnyCodable
-
 public class PlistManager {
     
     public typealias PlistName = String
@@ -9,77 +7,30 @@ public class PlistManager {
         return paths[0].appendingPathComponent("\(name).plist")
     }
     
-    private static func createFileIfNeeded(_ path: URL) {
-        if !FileManager.default.fileExists(atPath: path.path) {
-            let emptyDict = NSDictionary(dictionary: [String: String]())
-            emptyDict.write(toFile: path.path, atomically: true)
-        }
-    }
-    
-    private static func getPlistContents(for path: URL) -> Result<[String: AnyCodable], Error> {
-        createFileIfNeeded(path)
+    public static func get<T: Codable>(_ type: T.Type, from plistName: PlistName, handleError: ((Error?) -> Void)? = nil) -> T? {
+        let path = plistPath(plistName)
         do {
             let data = try Data(contentsOf: path)
             let decoder = PropertyListDecoder()
-            let dict = try decoder.decode([String: AnyCodable].self, from: data)
-            return(.success(dict))
+            let object = try decoder.decode(T.self, from: data)
+            handleError?(nil)
+            return object
         } catch {
-            return(.failure(error))
-        }
-    }
-    
-    public static func getObject(_ key: String, from plistName: PlistName, handleError: ((Error?) -> Void)? = nil) -> Codable? {
-        let path = plistPath(plistName)
-        switch getPlistContents(for: path) {
-        case .failure(let error):
             handleError?(error)
             return nil
-        case .success(let plistContents):
-            if let object = plistContents[key]?.value as? Codable {
-                handleError?(nil)
-                return object
-            } else {
-                handleError?(NSError()) //TODO: Make this a custom error
-                return nil
-            }
         }
     }
     
-    public static func remove(for key: String, from plistName: PlistName, handleError: ((Error?) -> Void)? = nil) {
+    public static func save<T: Codable>(_ object: T?, plistName: PlistName, handleError: ((Error?) -> Void)? = nil) {
         let path = plistPath(plistName)
-        switch getPlistContents(for: path) {
-        case .failure(let error):
+        let encoder = PropertyListEncoder()
+        encoder.outputFormat = .xml
+        do {
+            let data = try encoder.encode(object)
+            try data.write(to: path)
+            handleError?(nil)
+        } catch {
             handleError?(error)
-        case .success(var plistContents):
-            plistContents.removeValue(forKey: key)
-            let encoder = PropertyListEncoder()
-            encoder.outputFormat = .xml
-            do {
-                let data = try encoder.encode(plistContents)
-                try data.write(to: path)
-                handleError?(nil)
-            } catch {
-                handleError?(error)
-            }
-        }
-    }
-    
-    public static func saveValue(_ value: Codable, key: String, from plistName: PlistName, handleError: ((Error?) -> Void)? = nil) {
-        let path = plistPath(plistName)
-        switch getPlistContents(for: path) {
-        case .failure(let error):
-            handleError?(error)
-        case .success(var plistContents):
-            plistContents[key] = AnyCodable(value)
-            let encoder = PropertyListEncoder()
-            encoder.outputFormat = .xml
-            do {
-                let data = try encoder.encode(plistContents)
-                try data.write(to: path)
-                handleError?(nil)
-            } catch {
-                handleError?(error)
-            }
         }
     }
 
