a4f117fa403b73cc25c02986a76ced6061f66ff7
janakmshah <janakshah90@gmail.com>
2022-01-27 13:43:04 +0000

    add support for App Group containers

--- PlistManager.podspec
+++ PlistManager.podspec
@@ -5,7 +5,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'PlistManager'
-  s.version          = '1.1.1'
+  s.version          = '1.2.1'
   s.summary          = 'Lightweight plist data management framework, leveraging Codable in Swift'
 
 # This description is used to generate tags and improve search results.
--- PlistManager/Classes/PlistManager.swift
+++ PlistManager/Classes/PlistManager.swift
@@ -2,36 +2,63 @@ public class PlistManager {
     
     public typealias PlistName = String
     
-    private static func plistPath(_ name: PlistName) -> URL {
+    private static func baseDirectoryPath(_ name: PlistName) -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0].appendingPathComponent("\(name).plist")
     }
     
-    public static func get<T: Codable>(_ type: T.Type, from plistName: PlistName, handleError: ((Error?) -> Void)? = nil) -> T? {
-        let path = plistPath(plistName)
-        do {
-            let data = try Data(contentsOf: path)
-            let decoder = PropertyListDecoder()
-            let object = try decoder.decode(T.self, from: data)
-            handleError?(nil)
-            return object
-        } catch {
-            handleError?(error)
-            return nil
+    private static func appGroupDirectoryPath(appGroupId: String, name: PlistName) throws -> URL {
+        guard let path = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroupId) else {
+            throw(PlistManagerError.appGroupDirectoryNotFound(appGroupId))
         }
+        return path.appendingPathComponent("\(name).plist")
     }
     
-    public static func save<T: Codable>(_ object: T?, plistName: PlistName, handleError: ((Error?) -> Void)? = nil) {
-        let path = plistPath(plistName)
+    public static func get<T: Codable>(_ type: T.Type,
+                                       from plistName: PlistName,
+                                       appGroupId: String? = nil) throws -> T? {
+        
+        let path: URL
+        if let appGroupId = appGroupId {
+            path = try appGroupDirectoryPath(appGroupId: appGroupId, name: plistName)
+        } else {
+            path = baseDirectoryPath(plistName)
+        }
+        
+        let data = try Data(contentsOf: path)
+        let decoder = PropertyListDecoder()
+        let object = try decoder.decode(T.self, from: data)
+        return object
+    }
+    
+    public static func save<T: Codable>(_ object: T?,
+                                        plistName: PlistName,
+                                        appGroupId: String? = nil) throws {
         let encoder = PropertyListEncoder()
         encoder.outputFormat = .xml
-        do {
-            let data = try encoder.encode(object)
-            try data.write(to: path)
-            handleError?(nil)
-        } catch {
-            handleError?(error)
+        
+        let path: URL
+        if let appGroupId = appGroupId {
+            path = try appGroupDirectoryPath(appGroupId: appGroupId, name: plistName)
+        } else {
+            path = baseDirectoryPath(plistName)
         }
+        
+        let data = try encoder.encode(object)
+        try data.write(to: path)
     }
 
+}
+
+public enum PlistManagerError: Error {
+    case appGroupDirectoryNotFound(String)
+}
+
+extension PlistManagerError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .appGroupDirectoryNotFound(let appGroupId):
+            return NSLocalizedString("Could not find App Group Container with id: \(appGroupId)", comment: "Unable to find App Group Directory")
+        }
+    }
 }
